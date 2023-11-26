60982b1bef66bb42feaa61e6c68462645297a21c
DevDec <33901712+DevDec@users.noreply.github.com>
2023-11-26 12:09:40 +0000

    Test error

--- lua/transmit/init.lua
+++ lua/transmit/init.lua
@@ -1,7 +1,15 @@
 local M = {}
 
 M.send_file = function ()
-    vim.fn.jobstart({ "sftp", '/', '/' })
+    vim.fn.jobstart({ "sftp", '/', '/' }, {
+            on_stderr = function(_, data, _)
+                vim.print('error')
+                -- vim.list_extend(stderr, data)
+            end,
+            on_exit = function(_, code, _)
+                vim.print('failed')
+            end
+    })
 end
 
 return M;
