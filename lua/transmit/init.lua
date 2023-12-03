ca98894081f7c18e1950d5a7dba18c29f5a98099
DevDec <33901712+DevDec@users.noreply.github.com>
2023-12-03 13:32:04 +0000

    Initial commit with working upload and download of static file
    I've modified all of the transfer function to pure SFTP rather than using a mix of sftp, scp and ssh.
    
    Going forward I want all transfers to be done over SFTP, without relying on SSH because some users may not have ssh access.

--- lua/transmit/init.lua
+++ lua/transmit/init.lua
@@ -1,15 +1,103 @@
 local M = {}
+local data_path = vim.fn.stdpath("data")
 
-M.send_file = function ()
-    vim.fn.jobstart({ "sftp", '/', '/' }, {
-            on_stderr = function(_, data, _)
-                vim.print('error')
-                -- vim.list_extend(stderr, data)
+local server_config = {}
+local selected_server_mappings = {
+    aim_dev = "declanb"
+}
+
+local function sftp_connect(server)
+    local host = server_config[server].credentials.host
+    local username = server_config[server].credentials.username
+    local identity_file = server_config[server].credentials.identity_file
+    local remote_path = server_config[server].mappings[selected_server_mappings[server]].remote_path
+
+    return vim.fn.jobstart(
+        {
+            'sftp',
+            '-i',
+            identity_file,
+            '-o',
+            'UpdateHostKeys=no',
+            username .. '@' .. host .. ':' .. remote_path,
+        },
+        {
+            on_stdout = function (chanid, data, _)
+                for _, value in pairs(data) do
+                    if string.match(value, 'Connected') then
+                        return chanid
+                    end
+                end
             end,
-            on_exit = function(_, code, _)
-                vim.print('failed')
-            end
-    })
+            on_stderr = function (chanid, data, _)
+                for _, value in pairs(data) do
+                    if string.match(value, 'Connected') then
+                        return chanid
+                    end
+                end
+            end,
+        }
+    )
 end
+
+local function delete_remote_file(chanid, server)
+    local remove_string = 'rm composer.json \n'
+
+    vim.fn.chansend(chanid, remove_string)
+end
+
+local function upload_file(chanid, server)
+    local local_path = server_config[server].mappings[selected_server_mappings[server]].local_path
+
+    vim.fn.chansend(chanid, 'put ' .. local_path .. 'composer.json \n')
+end
+
+function M.send_file(server)
+    local chanid = sftp_connect('aim_dev')
+
+    local local_path = server_config[server].mappings[selected_server_mappings[server]].local_path
+
+    local f = io.open(local_path .. 'composer.json', "r")
+    local file_exists =  f ~= nil and io.close(f)
+
+    if file_exists == false then
+        delete_remote_file(chanid, 'aim_dev')
+    else
+        upload_file(chanid, 'aim_dev')
+    end
+
+    vim.fn.chansend(chanid, 'exit \n')
+end
+
+function M.setup(config)
+    if next(config) == nil then
+        return
+    end
+
+    server_config = config
+end
+
+M.setup({
+    aim_dev = {
+        credentials = {
+            host = "52.87.27.131",
+            username = "declan.brown",
+            identity_file = "~/.ssh/id_declanb"
+        },
+        mappings =  {
+            declanb = {
+                local_path = "/Volumes/T7/storeVisionPep/",
+                -- local_path = "~/projects/storeVisionPep/",
+                remote_path = "workspace.declanb"
+            },
+            declanb2 = {
+                local_path = "~/projects/storeVisionPep/",
+                remote_path = "E:/sites/workspace.declanb2/"
+            },
+        },
+    }
+})
+
+M.send_file('aim_dev')
 
 return M;
